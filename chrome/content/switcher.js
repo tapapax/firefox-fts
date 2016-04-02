e5844ef7883ca7bdd7b184d264ab93167c963027
a.volynskii <a.volynskii@corp.mail.ru>
2016-04-02 16:04:14 +0300

    Refactor switcher.js a bit

--- chrome/content/switcher.js
+++ chrome/content/switcher.js
@@ -4,83 +4,92 @@ Components.utils.import("resource://gre/modules/Services.jsm");
 Components.utils.import("chrome://tabswitcher/content/preferences.jsm");
 Components.utils.import("chrome://tabswitcher/content/actions_filter_worker.jsm");
 
-var resultsListbox;
-var patternTextbox;
-var progressImage;
-
-var actionsFilter = new ActionsFilterWorker;
-var filteringTimeout;
-
 ///////////////////////////////////////////////////////////////
 
-function init() {
-	resultsListbox = document.getElementById("results-listbox");
-	patternTextbox = document.getElementById("pattern-textbox");
-	progressImage = document.getElementById("progress-image");
+var Switcher = { };
 
-	patternTextbox.focus();
-	patternTextbox.addEventListener("keypress", function(e) {
-		if ([33, 34, 38, 40].indexOf(e.keyCode) !== -1) {
-			var event;
-			try {
-				event = new KeyboardEvent("keypress", e);
-			} catch (ex) {
-				event = document.createEvent("KeyboardEvent");
-				event.initKeyEvent("keypress", true, true, null, false, false, false,
-					false, e.keyCode, 0);
-			}
-			resultsListbox.dispatchEvent(event);
-			e.preventDefault();
-		}
-	});
+Switcher.init = function() {
+	this.resultsListbox = document.getElementById("results-listbox");
+	this.patternTextbox = document.getElementById("pattern-textbox");
+	this.progressImage = document.getElementById("progress-image");
+	this.performCommand = document.getElementById("perform-command");
+
+	this.patternTextbox.focus();
+	this.addListeners();
+
+	this.actionsFilter = new ActionsFilterWorker;
+	this.filterResults();
+};
+
+Switcher.addListeners = function() {
+	this.performCommand.addEventListener("command", this.performAction.bind(this));
+	this.patternTextbox.addEventListener("input", this.filterResults.bind(this));
+	this.patternTextbox.addEventListener("keypress", this.textboxListener.bind(this));
 
 	window.addEventListener("deactivate", () => window.close());
+};
 
-	filterResults();
-}
+Switcher.textboxListener = function(e) {
+	if ([33, 34, 38, 40].indexOf(e.keyCode) !== -1) {
+		var event;
+		try {
+			event = new KeyboardEvent("keypress", e);
+		} catch (ex) {
+			event = document.createEvent("KeyboardEvent");
+			event.initKeyEvent("keypress", true, true, null, false, false, false,
+				false, e.keyCode, 0);
+		}
+		this.resultsListbox.dispatchEvent(event);
+		e.preventDefault();
+	}
+};
 
-function filterResults() {
-	if (filteringTimeout) {
-		clearTimeout(filteringTimeout);
+Switcher.stopFiltering = function() {
+	if (this.filteringTimeout) {
+		clearTimeout(this.filteringTimeout);
+	}
+};
+
+Switcher.filterResults = function() {
+	this.stopFiltering();
+
+	while (this.resultsListbox.getRowCount()) {
+		this.resultsListbox.removeItemAt(0);
 	}
 
-	while (resultsListbox.getRowCount()) {
-		resultsListbox.removeItemAt(0);
-	}
+	this.actionsFilter.startSearch(this.patternTextbox.value);
+	this.doFilterSome();
+	this.resultsListbox.selectedIndex = 0;
+};
 
-	actionsFilter.startSearch(patternTextbox.value);
-
-	doFilterSome();
-
-	resultsListbox.selectedIndex = 0;
-}
-
-function doFilterSome() {
+Switcher.doFilterSome = function() {
 	const MAX_RESULTS = 20;
 	const ACTIONS_TO_PROCEED = 100;
 	var filtered = [];
-	var moreActions = actionsFilter.work(ACTIONS_TO_PROCEED, filtered);
+	var moreActions = this.actionsFilter.work(ACTIONS_TO_PROCEED, filtered);
 	for (var item of filtered) {
-		var listitem = addItemToList(resultsListbox, item.columnsText);
+		var listitem = addItemToList(this.resultsListbox, item.columnsText);
 		listitem.actionData = item;
-		listitem.addEventListener("dblclick", performAction);
+		listitem.addEventListener("dblclick", this.performAction.bind(this));
 	}
 
-	var inProgress = resultsListbox.getRowCount() < MAX_RESULTS && moreActions;
-	progressImage.hidden = !inProgress;
+	var inProgress = this.resultsListbox.getRowCount() < MAX_RESULTS && moreActions;
+	this.progressImage.hidden = !inProgress;
 	if (inProgress) {
-		filteringTimeout = setTimeout(doFilterSome, 0);
+		this.filteringTimeout = setTimeout(this.doFilterSome.bind(this), 0);
 	} else if (moreActions) {
-		addItemToList(resultsListbox, ["..."]);
+		addItemToList(this.resultsListbox, ["..."]);
 	}
-}
+};
 
-function performAction() {
-	try {
-		resultsListbox.selectedItem.actionData.perform();
+Switcher.performAction = function() {
+	var selectedItem = this.resultsListbox.selectedItem;
+	var ad = selectedItem ? selectedItem.actionData : null;
+	if (ad) {
+		ad.perform();
 		window.close();
-	} catch (e) { }
-}
+	}
+};
 
 function addItemToList(list, cells) {
 	var item = document.createElement("listitem");
