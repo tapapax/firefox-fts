ccd553ff87ac6c258d884ae6925fad16df8037dc
a.volynskii <a.volynskii@corp.mail.ru>
2018-03-03 13:41:12 +0300

    Remove legacy sources

--- chrome/content/defaultprefs.js
+++
@@ -1,5 +0,0 @@
-pref("extensions.tabswitcher@volinsky.net.openkey.key", " ");
-pref("extensions.tabswitcher@volinsky.net.openkey.shift", false);
-pref("extensions.tabswitcher@volinsky.net.openkey.alt", false);
-pref("extensions.tabswitcher@volinsky.net.openkey.control", true);
-pref("extensions.tabswitcher@volinsky.net.bookmarksActions", true);
