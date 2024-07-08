af2537c02b29ed23e20ca46df5d7df45019a47af
ElmourikiYassine <yassineelmouriki@gmail.com>
2024-07-08 02:12:24 +0100

    :)

--- tab_switcher/switcher.js
+++ tab_switcher/switcher.js
@@ -63,28 +63,6 @@ function updateVisibleTabs(query, preserveSelectedTabIndex) {
 		}
 	}
 
-	// $('#tabs_table tbody').empty().append(
-	// 	tabs.map((tab, tabIndex) =>
-	// 		$('<tr></tr>').append(
-	// 			$('<td></td>').append(
-	// 				tab.favIconUrl
-	// 					? $('<img width="16" height="16">')
-	// 						.attr('src',
-	// 							!tab.incognito
-	// 								? tab.favIconUrl
-	// 								: '/icons/mask16.svg'
-	// 						)
-	// 					: null
-	// 			),
-	// 			$('<td></td>').text(tab.title),
-	// 			$('<td></td>').text(tab.url)
-	// 		)
-	// 			.data('index', tabIndex)
-	// 			.data('tabId', tab.id)
-	// 			.on('click', () => setSelectedString(tabIndex))
-	// 			.on('dblclick', e => activateTab())
-	// 	)
-	// );
 
 	const tbody = document.getElementById('tabs_table').querySelector('tbody');
 	tbody.innerHTML = ''; // Clear existing content
@@ -138,16 +116,6 @@ function tabsFilter(query) {
 			|| (tab.title || '').toLowerCase().indexOf(pattern) !== -1);
 }
 
-// async function beginSetTabKeyword() {
-// 	isSettingKeyword = true;
-// 	const tabs = await browser.tabs.query({ active: true, currentWindow: true });
-// 	const keyword = await browser.sessions.getTabValue(tabs[0].id, "keyword");
-// 	$("#tabs_table__container").hide();
-// 	$("#keyword_label").show();
-// 	$("#search_input").attr("aria-labelledby", "keyword_label")
-// 		.val(keyword)
-// 		.select();
-// }
 async function beginSetTabKeyword() {
 	isSettingKeyword = true;
 	const tabs = await browser.tabs.query({ active: true, currentWindow: true });
@@ -171,12 +139,6 @@ async function beginSetTabKeyword() {
 }
 
 
-// async function setTabKeyword() {
-// 	const tabs = await browser.tabs.query({ active: true, currentWindow: true });
-// 	let keyword = $('#search_input').val();
-// 	await browser.sessions.setTabValue(tabs[0].id, "keyword", keyword);
-// 	window.close();
-// }
 
 async function setTabKeyword() {
 	const tabs = await browser.tabs.query({ active: true, currentWindow: true });
@@ -185,138 +147,7 @@ async function setTabKeyword() {
 	window.close();
 }
 
-reloadTabs();
 
-// $('#search_input')
-// 	.focus()
-// 	.on('input', event => {
-// 		if (isSettingKeyword) {
-// 			return;
-// 		}
-// 		if (event.target.value == "=") {
-// 			beginSetTabKeyword();
-// 		} else {
-// 			updateVisibleTabs(event.target.value, false);
-// 		}
-// 	});
-
-document.getElementById('search_input').addEventListener('input', function(event) {
-	if (isSettingKeyword) {
-		return;
-	}
-	if (event.target.value === "=") {
-		beginSetTabKeyword();
-	} else {
-		updateVisibleTabs(event.target.value, false);
-	}
-});
-
-// Focus on search input
-document.getElementById('search_input').focus();
-
-enableQuickSwitch();
-
-// $(window).on('keydown', event => {
-// 	const key = event.originalEvent.key;
-//
-// 	if ((key === 'ArrowDown') ||
-// 		(event.ctrlKey && key === 'n')) {
-// 		setSelectedString(getNextPageDownIndex(1));
-// 		event.preventDefault();
-// 	} else if ((key === 'ArrowUp') ||
-// 		(event.ctrlKey && key === 'p')) {
-// 		setSelectedString(getNextPageUpIndex(1));
-// 		event.preventDefault();
-// 	} else if (key === 'PageDown') {
-// 		setSelectedString(getNextPageDownIndex(13));
-// 		event.preventDefault();
-// 	} else if (key === 'PageUp') {
-// 		setSelectedString(getNextPageUpIndex(13));
-// 		event.preventDefault();
-// 	} else if (key === 'Escape') {
-// 		window.close();
-// 	} else if (key === 'Enter') {
-// 		if (isSettingKeyword) {
-// 			setTabKeyword();
-// 		} else {
-// 			activateTab();
-// 		}
-// 	} else if ((event.ctrlKey && key === 'Delete') ||
-// 		(event.metaKey && key === 'Backspace')) {
-// 		closeTab();
-// 		event.preventDefault();
-// 	}
-// });
-
-window.addEventListener('keydown', event => {
-	const key = event.key;
-
-	if ((key === 'ArrowDown') ||
-		(event.ctrlKey && key === 'n')) {
-		setSelectedString(getNextPageDownIndex(1));
-		event.preventDefault();
-	} else if ((key === 'ArrowUp') ||
-		(event.ctrlKey && key === 'p')) {
-		setSelectedString(getNextPageUpIndex(1));
-		event.preventDefault();
-	} else if (key === 'PageDown') {
-		setSelectedString(getNextPageDownIndex(13));
-		event.preventDefault();
-	} else if (key === 'PageUp') {
-		setSelectedString(getNextPageUpIndex(13));
-		event.preventDefault();
-	} else if (key === 'Escape') {
-		window.close();
-	} else if (key === 'Enter') {
-		if (isSettingKeyword) {
-			setTabKeyword();
-		} else {
-			activateTab();
-		}
-	} else if ((event.ctrlKey && key === 'Delete') ||
-		(event.metaKey && key === 'Backspace')) {
-		closeTab();
-		event.preventDefault();
-	}
-});
-
-// function enableQuickSwitch() {
-// 	const States = {
-// 		pending: 0,
-// 		enabled: 1,
-// 		disabled: 2,
-// 	};
-//
-// 	let state = States.pending;
-//
-// 	$(window).on('keydown', event => {
-// 		const key = event.originalEvent.key;
-//
-// 		if (key === ' ' && state !== States.disabled && event.ctrlKey) {
-// 			state = States.enabled;
-// 			const stringToSelect = event.shiftKey
-// 				? getNextPageUpIndex(1)
-// 				: getNextPageDownIndex(1);
-// 			setSelectedString(stringToSelect);
-// 			event.preventDefault();
-// 		}
-// 		if (key === 'Control') {
-// 			state = States.disabled;
-// 		}
-// 	});
-//
-// 	$(window).on('keyup', event => {
-// 		const key = event.originalEvent.key;
-//
-// 		if (key === 'Control') {
-// 			if (state === States.enabled) {
-// 				activateTab();
-// 			} else {
-// 				state = States.disable;
-// 			}
-// 		}
-// 	});
-// }
 
 function enableQuickSwitch() {
 	const States = {
@@ -356,16 +187,6 @@ function enableQuickSwitch() {
 		}
 	});
 }
-// function setSelectedString(index) {
-// 	const table = $('#tabs_table tbody');
-// 	const selector = `tr:nth-child(${index + 1})`;
-//
-// 	if (selectedString) {
-// 		selectedString.removeClass('tabs_table__selected');
-// 	}
-// 	selectedString = table.find(selector).addClass('tabs_table__selected');
-// 	scrollIntoView();
-// }
 
 
 function setSelectedString(index) {
@@ -384,14 +205,6 @@ function setSelectedString(index) {
 	scrollIntoView();
 }
 
-// function getSelectedTabIndex() {
-// 	const selected = $('#tabs_table .tabs_table__selected');
-// 	if (selected.length === 0) {
-// 		return 0;
-// 	} else {
-// 		return selected.data('index');
-// 	}
-// }
 
 function getSelectedTabIndex() {
 	const selected = document.querySelector('#tabs_table .tabs_table__selected');
@@ -402,11 +215,6 @@ function getSelectedTabIndex() {
 	}
 }
 
-// function getNextPageDownIndex(stepSize) {
-// 	const tabIndex = getSelectedTabIndex();
-// 	const numVisibleTabs = $('#tabs_table tbody tr').length;
-// 	return (tabIndex + stepSize) % numVisibleTabs;
-// }
 
 function getNextPageDownIndex(stepSize) {
 	const tabIndex = getSelectedTabIndex();
@@ -414,11 +222,6 @@ function getNextPageDownIndex(stepSize) {
 	return (tabIndex + stepSize) % numVisibleTabs;
 }
 
-// function getNextPageUpIndex(stepSize) {
-// 	const tabIndex = getSelectedTabIndex();
-// 	const numVisibleTabs = $('#tabs_table tbody tr').length;
-// 	return (tabIndex - stepSize + numVisibleTabs) % numVisibleTabs;
-// }
 
 function getNextPageUpIndex(stepSize) {
 	const tabIndex = getSelectedTabIndex();
@@ -426,21 +229,6 @@ function getNextPageUpIndex(stepSize) {
 	return (tabIndex - stepSize + numVisibleTabs) % numVisibleTabs;
 }
 
-// function scrollIntoView() {
-// 	const container = $('#tabs_table__container');
-// 	const scrollTop = container.scrollTop();
-// 	const scrollBottom = scrollTop + container.height();
-//
-// 	const elem = selectedString;
-// 	const elemTop = elem.position().top;
-// 	const elemBottom = elemTop + elem.height();
-//
-// 	if (elemBottom > scrollBottom) {
-// 		container.scrollTop(scrollTop + elemBottom - scrollBottom);
-// 	} else if (elemTop < 0) {
-// 		container.scrollTop(scrollTop + elemTop);
-// 	}
-// }
 
 function scrollIntoView() {
 	const container = document.getElementById('tabs_table__container');
@@ -458,29 +246,6 @@ function scrollIntoView() {
 	}
 }
 
-// async function activateTab() {
-// 	const tabId = selectedString.data('tabId');
-// 	const tab = await browser.tabs.get(tabId);
-// 	await browser.tabs.update(tab.id, { active: true });
-// 	await browser.windows.update(tab.windowId, { focused: true });
-// 	window.close();
-// }
-
-// async function activateTab() {
-// 	const tabId = selectedString.data('tabId');
-// 	const tab = await browser.tabs.get(parseInt(tabId));
-//
-// 	if (tab) {
-// 		await browser.tabs.update(tab.id, { active: true });
-//
-// 		const currentWin = await browser.windows.getCurrent();
-// 		if (currentWin.id !== tab.windowId) {
-// 			await browser.windows.update(tab.windowId, { focused: true });
-// 		} else {
-// 			window.close();
-// 		}
-// 	}
-// }
 
 async function activateTab() {
 	// Ensure selectedString is defined and has the required data attribute
@@ -501,12 +266,6 @@ async function activateTab() {
 	}
 }
 
-// async function closeTab() {
-// 	const tabId = selectedString.data('tabId');
-// 	await browser.tabs.remove(tabId);
-// 	reloadTabs();
-// }
-
 async function closeTab() {
 	// Ensure selectedString is defined and has the required data attribute
 	if (selectedString && selectedString.dataset.tabId) {
@@ -515,4 +274,62 @@ async function closeTab() {
 		reloadTabs();
 	}
 }
+
+// Add DOMContentLoaded event listener for window
+//
+
+window.addEventListener("DOMContentLoaded", () => {
+	console.log("HIIIIIIIIIIIIIIIIIIIIIIII");
+	reloadTabs();
+
+	document.getElementById('search_input').addEventListener('input', function(event) {
+		if (isSettingKeyword) {
+			return;
+		}
+		if (event.target.value === "=") {
+			beginSetTabKeyword();
+		} else {
+			updateVisibleTabs(event.target.value, false);
+		}
+	});
+
+	// Focus on search input
+	document.getElementById('search_input').focus();
+
+	enableQuickSwitch();
+
+
+	window.addEventListener('keydown', event => {
+		const key = event.key;
+
+		if ((key === 'ArrowDown') ||
+			(event.ctrlKey && key === 'n')) {
+			setSelectedString(getNextPageDownIndex(1));
+			event.preventDefault();
+		} else if ((key === 'ArrowUp') ||
+			(event.ctrlKey && key === 'p')) {
+			setSelectedString(getNextPageUpIndex(1));
+			event.preventDefault();
+		} else if (key === 'PageDown') {
+			setSelectedString(getNextPageDownIndex(13));
+			event.preventDefault();
+		} else if (key === 'PageUp') {
+			setSelectedString(getNextPageUpIndex(13));
+			event.preventDefault();
+		} else if (key === 'Escape') {
+			window.close();
+		} else if (key === 'Enter') {
+			if (isSettingKeyword) {
+				setTabKeyword();
+			} else {
+				activateTab();
+			}
+		} else if ((event.ctrlKey && key === 'Delete') ||
+			(event.metaKey && key === 'Backspace')) {
+			closeTab();
+			event.preventDefault();
+		}
+	});
+
+});
 
